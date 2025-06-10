2acde94531ddf5942036007c075851bf31328b27
haafiidzh <hafid.kusuma45@gmail.com>
2025-06-10 23:37:07 +0700

    update db

--- koneksi.js
+++ koneksi.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const koneksi = new Sequelize('sidb', 'root', '',{
+const koneksi = new Sequelize('sidb', 'root', '12345678',{
     host : 'localhost',
     dialect : 'mysql'
 });
